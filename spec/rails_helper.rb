8fff2a0ee92c2d2ed0d49f238cd108e5a03b30bb
Stirling Hostetter <stirl@pop-os.localdomain>
2022-07-12 00:17:56 -0500

    Rails helper configured

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'simplecov'
-Simplecov.start 'rails'
+SimpleCov.start 'rails'
 
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
@@ -64,4 +64,5 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include FactoryBot::Syntax::Methods
 end
