dbd4b261eb283efcc50db0a1d59a8af5203eec3e
Stirling Hostetter <stirl@pop-os.localdomain>
2022-07-12 00:17:07 -0500

    Test: Testing that Merchants exist correctly

---
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchants API' do
+  it 'sends a list of merchants' do
+    create_list(:merchant, 3)
+
+    get '/api/v1/merchants'
+
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    merchants.each do |merchant|
+      expect(merchant).to have_key(:id)
+      expect(merchant[:id]).to be_an(Integer)
+
+      expect(merchant).to have_key(:name)
+      expect(merchant[:name]).to be_an(String)
+    end
+  end
+end
