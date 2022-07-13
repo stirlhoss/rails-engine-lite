e54215741e360b0b69c0d7c8d5c668efcab8a57d
Stirling Hostetter <stirl@pop-os.localdomain>
2022-07-13 16:21:33 -0500

    Add merchant Id to Item serializer

--- app/serializer/item_serializer.rb
+++ app/serializer/item_serializer.rb
@@ -2,5 +2,6 @@ class ItemSerializer
   include JSONAPI::Serializer
   attributes :name,
              :description,
-             :unit_price
+             :unit_price,
+             :merchant_id
 end
