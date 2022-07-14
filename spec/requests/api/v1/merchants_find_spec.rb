c153ba097701189cbd2b7c619240b9df8209508f
Stirling Hostetter <stirl@pop-os.localdomain>
2022-07-14 16:51:44 -0500

    Test: sad path and error test

--- spec/requests/api/v1/merchants_find_spec.rb
+++ spec/requests/api/v1/merchants_find_spec.rb
@@ -19,13 +19,23 @@ RSpec.describe 'find for merchant endpoint' do
       expect(merchant[:data][:attributes]).to have_key(:name)
       expect(merchant[:data][:attributes][:name]).to eq('Pilson & Sons')
     end
+
+    it 'raises a 400 error if there are no params' do
+      get api_v1_merchants_find_path, params: { name: ''}
+
+      expect(response.status).to eq(400)
+    end
   end
 
   describe 'sad path' do
     it 'raises error when there is no match' do
-      get '/api/v1/merchants/find', params: { name: '@' }
+      get api_v1_merchants_find_path, params: { name: 'X' }
 
-      expect(response.status).to eq(204)
+      expect(response).to be_successful
+
+      merchant = JSON.parse(response.body, symbolize_names: true)
+
+      expect(merchant[:errors]).to eq("Could not find merchant that matched with X")
     end
   end
 end
