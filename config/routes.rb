4bc822c371e995161af280e781eee29d2f3a41d3
Stirling Hostetter <stirl@pop-os.localdomain>
2022-07-14 00:37:47 -0500

    Routing: Item _merchants route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
       resources :merchants, only: [:index, :show] do
         resources :items, only: [:index], controller: 'merchant_items'
       end
-      resources :items
+      resources :items do
+        get '/merchant', controller: :item_merchants, action: :show
+      end
     end
   end
 end
