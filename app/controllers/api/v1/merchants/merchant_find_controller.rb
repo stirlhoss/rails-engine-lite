211b3936e6d1c4443d17a5a229d8dfe02d20681f
Stirling Hostetter <stirl@pop-os.localdomain>
2022-07-14 16:51:00 -0500

    Update find controller
    it now responds when there is no input and when there are no matches

--- app/controllers/api/v1/merchants/merchant_find_controller.rb
+++ app/controllers/api/v1/merchants/merchant_find_controller.rb
@@ -1,12 +1,15 @@
 class Api::V1::Merchants::MerchantFindController < ApplicationController
   def find
     merchant = Merchant.find_by_name(params[:name])
-    if merchant.empty?
-      render json: { errors: { data: 'no match'} }, status: 400
+    if !merchant.empty?
+      render json: MerchantSerializer.new(merchant.first)
+
+    elsif params[:name].blank?
+      render json: { errors: { data: 'no search parameters' } }, status: 400
 
     else
+      render json: MerchantSerializer.no_match(params[:name])
 
-      render json: MerchantSerializer.new(merchant.first)
     end
   end
 end
