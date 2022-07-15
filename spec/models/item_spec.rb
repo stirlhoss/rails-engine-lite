eecf9b94d42a477f1dbd4cee6f7d9c2790ca5099
Stirling Hostetter <stirlhoss@proton.me>
2022-07-14 23:24:13 -0500

    Refactor and add test for find_all method

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe Item do
   describe 'relationships' do
     it { should belong_to(:merchant) }
-    it { should have_many(:invoice_items)}
-    it { should have_many(:invoices).through(:invoice_items)}
+    it { should have_many(:invoice_items) }
+    it { should have_many(:invoices).through(:invoice_items) }
   end
 
   describe 'validations' do
@@ -12,5 +12,17 @@ RSpec.describe Item do
     it { should validate_presence_of(:description) }
     it { should validate_presence_of(:unit_price) }
     it { should validate_presence_of(:merchant_id) }
+  end
+
+  describe 'class methods' do
+    it '#find_by_name finds all items that match a name' do
+      merchant = Merchant.create!(name: 'Bobby Brown')
+      item1 = create(:item, merchant_id: merchant.id, name: 'log')
+      item2 = create(:item, merchant_id: merchant.id, name: 'stick')
+      item3 = create(:item, merchant_id: merchant.id, name: 'tree')
+      item4 = create(:item, merchant_id: merchant.id, name: 'sloth')
+
+      expect(Item.find_by_name('lo')).to eq [item1, item4]
+    end
   end
 end
