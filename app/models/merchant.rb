6d9900bd76407217aede3e3a0c1ab5065c0c3eb1
Stirling Hostetter <stirlhoss@proton.me>
2022-07-21 14:40:31 -0500

    add relationships and AR

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,9 +1,23 @@
 class Merchant < ApplicationRecord
+  validates_presence_of :name
   has_many :items
+  has_many :invoices
+  has_many :invoice_items, through: :invoices
+  has_many :customers, through: :invoices
+  has_many :transactions, through: :invoices
 
   def self.find_by_name(name)
     where('name ILIKE ?', "%#{name}%")
       .order(:name)
       .limit(1)
+  end
+
+  def self.top_merchants_by_revenue(quantity)
+    joins(invoices: [:invoice_items, :transactions])
+      .where(transactions: { result: 'success' }, invoices: { status: 'shipped' })
+      .select(:name, :id, 'SUM(invoice_items.quantity * invoice_items.unit_price) as revenue')
+      .group(:id)
+      .order(revenue: :desc)
+      .limit(quantity)
   end
 end
